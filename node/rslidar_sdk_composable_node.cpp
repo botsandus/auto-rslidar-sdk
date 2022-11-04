01fb8f37d4674ce1e4574e47b4f59d21490c62bb
Guillaume Doisy <guillaume@botsandus.com>
2022-11-04 18:24:01 +0000

    remove copyright and add node options

--- node/rslidar_sdk_composable_node.cpp
+++ node/rslidar_sdk_composable_node.cpp
@@ -1,35 +1,3 @@
-/*********************************************************************************************************************
-Copyright (c) 2020 RoboSense
-All rights reserved
-
-By downloading, copying, installing or using the software you agree to this license. If you do not agree to this
-license, do not download, install, copy or use the software.
-
-License Agreement
-For RoboSense LiDAR SDK Library
-(3-clause BSD License)
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
-disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the names of the RoboSense, nor Suteng Innovation Technology, nor the names of other contributors may be used
-to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************************************************************/
-
 #include "manager/node_manager.hpp"
 
 #include <rs_driver/macro/version.hpp>
@@ -41,8 +9,8 @@ using namespace robosense::lidar;
 namespace rslidar_driver {
 class RslidarSdkComposableNode : public rclcpp::Node {
 public:
-  RslidarSdkComposableNode()
-  : Node("rslidar_sdk_composable_node")
+  RslidarSdkComposableNode(rclcpp::NodeOptions const & options)
+  : Node("rslidar_sdk_composable_node", options)
   {
     std::string config_path;
     config_path = (std::string)PROJECT_PATH;
@@ -85,7 +53,7 @@ int main(int argc, char** argv)
 {
   signal(SIGINT, signalHandler);
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<rslidar_driver::RslidarSdkComposableNode>();
+  auto node = std::make_shared<rslidar_driver::RslidarSdkComposableNode>(rclcpp::NodeOptions());
   rclcpp::spin(node->get_node_base_interface());
   rclcpp::shutdown();
 
